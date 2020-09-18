124ebd36d1a57b0cb58842071528f76b878ecd16
Debby Wu <yunying1203@gmail.com>
2020-09-18 15:03:37 +0800

    final

--- read.py
+++ read.py
@@ -18,3 +18,10 @@ for d in data:
         new.append(d)
 print("一共有", len(new), "筆留言長度小於100")
 print(new[0])
+
+good = []
+for d in data:
+    if "good" in d:
+        good.append(d)
+print("一共有", len(good),"筆留言提到good")
+print(good[0])
